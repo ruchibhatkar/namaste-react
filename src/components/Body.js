17207f081958a35ad86527f64c2766bc19799647
ruchibhatkar <bhatkarruchi@gmail.com>
2026-01-20 22:17:22 +0530

    episode-05-reactHooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  //Local state variable
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList); //Directly pass the mockData i.e. resList here
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurant(filteredList); //Pass the updated filter values to state variable
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((Restaurant) => (
+        {listOfRestaurant.map((Restaurant) => (
           <RestaurantCard key={Restaurant.info.id} resData={Restaurant} />
         ))}
       </div>
